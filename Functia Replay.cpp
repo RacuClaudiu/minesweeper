38c80a6675cc633dc3ed6c134bf7f7287a5fd688
RacuClaudiu <jasmin_racu@yahoo.com>
2017-01-13 08:31:10 +0200

    Add files via upload

---
+++ Functia Replay.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void reluare()
+{
+    int choice;
+    cout<<endl;
+    cout<<" 1) reia;"<<endl;
+    cout<<" 2) iesire;"<<endl;
+    cin>>choice;
+    switch(choice)
+    {
+    case 1:
+        main();
+    case 2:
+      break;
+    }
+}
+int main()
+{
+}
